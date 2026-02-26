e5053fa345d02f0b7aa8dbb010ab6af5ad060069
Germán Mora <dmm71317@gmail.com>
2026-02-25 20:12:22 -0600

    test: add Node.js test compatibility for group-anagrams
    - Created test.js with Node.js test module imports
    - Added eslint-env node directive
    - Imported describe and it from node:test
    - Added placeholder test case

---
+++ 01-arrays-strings/001-group-anagrams/test.js
@@ -0,0 +1,9 @@
+/* eslint-env node */
+const assert = require('assert');
+const { describe, it } = require('node:test');
+
+describe('001 Group Anagrams', () => {
+  it('placeholder', () => {
+    assert.strictEqual(true, true);
+  });
+});
